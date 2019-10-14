0ef40b0950f5bbf7f17ba545968b1d0e6142632b
ulanjava <56231858+ulanjava@users.noreply.github.com>
2019-10-14 14:52:12 +0600

    Add files via upload

---
+++ snproject/msUser/src/main/java/snproject/msUser/MsUserApplication.java
@@ -0,0 +1,18 @@
+package snproject.msUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class MsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserApplication.class, args);
+	}
+
+}
+
